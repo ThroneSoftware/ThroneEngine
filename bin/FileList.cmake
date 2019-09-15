6bdc271e8d7921205a52760389a04c843a1a53a6
FelixLamontagne2 <f.lamontagne.97@gmail.com>
2019-09-15 11:52:45 -0400

    Cleaned up the project a bit -The folder Build is now ignored -The folder bin/ is created to output runtimes and librairies

--- bin/FileList.cmake
+++
@@ -1,3 +0,0 @@
-target_sources(Throne PRIVATE 
-	CMakeFiles/3.15.3/CompilerIdCXX/CMakeCXXCompilerId.cpp
-)
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-
 #include <optional>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+string ExePath()
+{
+	char buffer[MAX_PATH];
+	GetModuleFileName(NULL, buffer, MAX_PATH);
+	string::size_type pos = string(buffer).find_last_of("\\/");
+	return string(buffer).substr(0, pos);
+}
 
 int main()
 {
-    std::cout << "Throne!" << std::endl;
+    std::cout << ExePath() << std::endl;
+	std::cout << "Throne!" << std::endl;
 
-    std::optional<int> opt;
+	std::optional<int> opt;
 }
