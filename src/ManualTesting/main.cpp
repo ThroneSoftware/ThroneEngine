f4a26407236ca925652f70c331fd5124cdd3df5a
FelixLamontagne2 <f.lamontagne.97@gmail.com>
2019-09-21 13:02:25 -0400

    asd

--- src/ManualTesting/main.cpp
+++ src/ManualTesting/main.cpp
@@ -11,7 +11,7 @@ string ExePath()
 	char buffer[MAX_PATH];
 	GetModuleFileName(NULL, buffer, MAX_PATH);
 	string::size_type pos = string(buffer).find_last_of("\\/");
-	return string(buffer).substr(0, pos);
+	return string(buffer).substr(0, pos);//
 }
 
 int main()
