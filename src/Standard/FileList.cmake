d5b205d8b49fa8952f7c9baeb011ed0dfbcccb77
Félix Lamontagne <f.lamontagne.97@gmail.com>
2020-01-12 21:34:38 -0500

    WIP

--- src/Standard/FileList.cmake
+++ src/Standard/FileList.cmake
@@ -2,7 +2,7 @@ target_sources(Standard PRIVATE
 	Algorithm.h
 	Aliases.h
 	CompressedPair.h
-	Fake.cpp
+	Manager.h
 	Pointers.h
 	Private/Resource.h
 )
---
+++ src/Standard/Manager.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "Pointers.h"
+
+#include <vector>
+
+namespace trs
+{
+	template <typename ObjectType>
+	class Manager
+	{
+	public:
+		using value_type = ObjectType;
+
+	private:
+		template <typename ObjectType>
+		class Notifier
+		{
+		public:
+			using value_type = ObjectType;
+
+		public:
+			Notifier(Manager<value_type>& manager)
+			  : m_manager(manager)
+			{
+			}
+
+			void operator()(value_type* ptr)
+			{
+				m_manager.erase(ptr);
+			}
+
+		private:
+			Manager<value_type>& m_manager;
+		};
+
+	public:
+		Manager() = default;
+
+		Manager(const Manager& other) = delete;
+		Manager& operator=(const Manager& other) = delete;
+
+		Manager(Manager&& other) = delete;
+		Manager& operator=(Manager&& other) = delete;
+
+		~Manager() = default;
+
+		template <typename... Args>
+		void emplace(Args&&... args)
+		{
+			auto& ref = m_pool.emplace_back(std::forward<Args>(args)...);
+
+			auto ptrOwner = trs::makePtrOwnerWithNotifier<value_type, Notifier>(Notifier(*this), &ref);
+			m_objects.emplace_back(std::move(ptrOwner));
+		}
+
+		void erase(value_type* ptr)
+		{
+			{
+				auto found = std::find_if(m_objects.begin(), m_objects.end(), [](PtrOwner<value_type>& ptrOwner) {
+					return ptrOwner.getPtr() == ptr;
+				});
+
+				m_objects.erase(found);
+			}
+
+			{
+				//auto found = std::find_if(m_pool.begin(), m_pool.end(), [](value_type& value) {
+				//	return &value == ptr;
+				//});
+
+				//m_pool.erase(found);
+			}
+		}
+
+	private:
+		std::vector<PtrOwner<value_type>> m_objects;
+
+		std::list<value_type> m_pool;  // temporary until we have a better solution, see issue #39
+	};
+}  // namespace trs
