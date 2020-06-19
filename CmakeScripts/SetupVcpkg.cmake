b9a59cc4031fd3298ef3c0d77008d759085664b8
Félix Lamontagne <f.lamontagne.97@gmail.com>
2020-06-18 22:21:47 -0400

    Small improvements to CMakeLists and vcpkg

--- CmakeScripts/SetupVcpkg.cmake
+++ CmakeScripts/SetupVcpkg.cmake
@@ -5,29 +5,21 @@ function(vcpkg_install_packages)
         COMMAND ${VCPKG_ROOT_FOLDER}/vcpkg install ${ARGN} --triplet ${VCPKG_TARGET_TRIPLET} --clean-after-build)
 endfunction()
 
-function(install_and_find_packages packages)
-    vcpkg_install_packages(${packages})
+function(find_packages packages)
     foreach(package ${packages})
         find_package(${package} CONFIG REQUIRED)
     endforeach()
 endfunction()
 
-function(install_and_find_non_generic_packages)
-    # Microsoft GSL
-    vcpkg_install_packages(ms-gsl)
+function(find_non_generic_packages)
     find_package(Microsoft.GSL CONFIG REQUIRED)
 
-    # Stb
-    vcpkg_install_packages(Stb)
     find_package(Stb REQUIRED)
 endfunction()
 
-function(install_and_find_components module packages components)
-    vcpkg_install_packages(${packages})
+function(find_components module components)
     find_package(${module} REQUIRED COMPONENTS ${components})
 endfunction()
-
-
 
 
 if(VCPKG_ROOT_FOLDER)
@@ -37,15 +29,15 @@ if(VCPKG_ROOT_FOLDER)
       
     include(${VCPKG_ROOT_FOLDER}/scripts/buildsystems/vcpkg.cmake)
     
-    # vcpkg packages
-    set(vcpkg_package_dependencies Catch2;glm;GTest;fmt;Freetype)
-    install_and_find_packages("${vcpkg_package_dependencies}")
-
-    install_and_find_non_generic_packages()
+    set(vcpkg_package_dependencies Catch2;glm;GTest;fmt;Freetype;ms-gsl;Stb;boost-container;boost-signals2;boost-algorithm)
+    vcpkg_install_packages("${vcpkg_package_dependencies}")
 
     # vcpkg packages
-    set(vcpkg_module_dependencies boost-container;boost-signals2;boost-algorithm)
+    set(packages Catch2;glm;GTest;fmt;Freetype)
+    find_packages("${packages}")
+    find_non_generic_packages()
+
     # Only libraries (header only should not be in there)
-    set(module_components container;)
-    install_and_find_components(Boost "${vcpkg_module_dependencies}" "${module_components}")
+    set(module_components container)
+    find_components(Boost "${module_components}")
 endif()
