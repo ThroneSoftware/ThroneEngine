cd2776cca113b7cf91ed4a423cd041220c685b72
Félix Lamontagne <f.lamontagne.97@gmail.com>
2020-08-30 22:01:13 -0400

    Return a boolean in applyIf

--- src/Utilities/Utility.h
+++ src/Utilities/Utility.h
@@ -12,29 +12,33 @@ namespace tru
 		return static_cast<DstType>(src);
 	}
 
-    /// <summary>
-    /// Apply a function on an atomic if the predicate returns true.
-    /// </summary>
-    template <typename Func, typename Predicate, typename AtomicType>
-    void applyIf(AtomicType& atomic, Func func, Predicate pred)
-    {
-        auto tempVal = atomic.load();
-        while (pred(tempVal))
-        {
-            if (atomic.compare_exchange_weak(tempVal, func(tempVal)))
-            {
-                break;
-            }
-        }
-    }
+	/// <summary>
+	/// Apply a function on an atomic if the predicate returns true.
+	/// </summary>
+	template <typename Func, typename Predicate, typename AtomicType>
+	bool applyIf(AtomicType& atomic, Func func, Predicate pred)
+	{
+		auto tempVal = atomic.load();
+		while(pred(tempVal))
+		{
+			if(atomic.compare_exchange_weak(tempVal, func(tempVal)))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 
-    template <auto comparedValue, typename AtomicType>
-    void incrementNotEqual(AtomicType& atomic)
-    {
-        applyIf(
-            atomic,
-            [](auto value) { return value + 1;},
-            [](auto value) { return value != comparedValue;}
-        );
-    }
+	template <auto comparedValue, typename AtomicType>
+	bool incrementNotEqual(AtomicType& atomic)
+	{
+		return applyIf(
+			atomic,
+			[](auto value) {
+				return value + 1;
+			},
+			[](auto value) {
+				return value != comparedValue;
+			});
+	}
 }  // namespace tru
