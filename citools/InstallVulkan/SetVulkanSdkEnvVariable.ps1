f1b1ce336f1175d202820db69931363d7c555482
Félix Lamontagne <f.lamontagne.97@gmail.com>
2021-03-11 17:08:05 -0500

    fix path

--- citools/InstallVulkan/SetVulkanSdkEnvVariable.ps1
+++ citools/InstallVulkan/SetVulkanSdkEnvVariable.ps1
@@ -1,9 +1,9 @@
 $vulkanSdkVar1 = "VULKAN_SDK"
 $vulkanSdkVar2 = "VK_SDK_PATH"
 
-$vulkanSdkVersion = Get-ChildItem -Path "C:\VulkanSdk\"
+$vulkanSdkVersion = Get-ChildItem -Path "C:\VulkanSDK\"
 
-$vulkanSdkPath = "C:\VulkanSdk\$vulkanSdkVersion"
+$vulkanSdkPath = "C:\VulkanSDK\$vulkanSdkVersion"
 
 Write-Host "##vso[task.setvariable variable=$($vulkanSdkVar1)]$($vulkanSdkPath)"
 Write-Host "##vso[task.setvariable variable=$($vulkanSdkVar2)]$($vulkanSdkPath)"
