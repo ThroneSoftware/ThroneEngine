93a3d6cebb7fd9eab9456aba5998c15f10c32301
Félix Lamontagne <f.lamontagne.97@gmail.com>
2021-07-11 11:26:15 -0400

    wip

--- src/Core/Math/Transform.cpp
+++ src/Core/Math/Transform.cpp
@@ -2,5 +2,136 @@
 
 namespace trc
 {
+	void Transform::lookAt(const Transform& target)
+	{
+		///
 
-}
+		resetCachedTransform();
+	}
+
+	void Transform::lookAt(const glm::vec3& target)
+	{
+		///
+
+		resetCachedTransform();
+	}
+
+	void Transform::rotate(const glm::quat& rotation, TransformSpace space)
+	{
+		///
+
+		resetCachedTransform();
+	}
+
+	void Transform::rotateOnAxis(const glm::vec3& axis, Radian angle, TransformSpace space)
+	{
+		///
+
+		resetCachedTransform();
+	}
+
+	void Transform::setRotation(const glm::quat& rotation)
+	{
+		m_rotation = rotation;
+
+		resetCachedTransform();
+	}
+
+	glm::quat Transform::getRotation() const
+	{
+		return m_rotation;
+	}
+
+	void Transform::translate(const glm::vec3& translation, TransformSpace space)
+	{
+		///
+
+		resetCachedTransform();
+	}
+
+	void Transform::setPosition(const glm::vec3& position)
+	{
+		m_position = position;
+
+		resetCachedTransform();
+	}
+
+	glm::vec3 Transform::getPosition() const
+	{
+		return m_position;
+	}
+
+	void Transform::scale(const glm::vec3& scale, ScalingMode mode)
+	{
+		switch(mode)
+		{
+			case trc::ScalingMode::Additive:
+			{
+				m_scale += scale;
+				break;
+			}
+			case trc::ScalingMode::Multiplicative:
+			{
+				m_scale *= scale;
+				break;
+			}
+			default:
+				break;
+		}
+
+		resetCachedTransform();
+	}
+
+	void Transform::setScale(const glm::vec3& scale)
+	{
+		m_scale = scale;
+
+		resetCachedTransform();
+	}
+
+	glm::vec3 Transform::getScale() const
+	{
+		return m_scale;
+	}
+
+	glm::vec3 Transform::right() const
+	{
+		return m_rotation * glm::vec3(1.0f, 0.0f, 0.0f);
+	}
+
+	glm::vec3 Transform::up() const
+	{
+		return m_rotation * glm::vec3(0.0f, 1.0f, 0.0f);
+	}
+
+	glm::vec3 Transform::forward() const
+	{
+		return m_rotation * glm::vec3(0.0f, 0.0f, 1.0f);
+	}
+
+	void Transform::updateCachedTransform()
+	{
+		m_cachedTransform.emplace(computeTransform());
+	}
+
+	glm::mat4 Transform::computeTransform() const
+	{
+		///
+
+		return glm::mat4();
+	}
+
+	glm::mat4& Transform::getCachedTransformRef()
+	{
+		if(!m_cachedTransform)
+		{
+			updateCachedTransform();
+		}
+		return *m_cachedTransform;
+	}
+
+	void Transform::resetCachedTransform()
+	{
+		m_cachedTransform.reset();
+	}
+}  // namespace trc
--- src/Core/Math/Transform.h
+++ src/Core/Math/Transform.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include "Angle.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+#include <optional>
+
 namespace trc
 {
 	enum class TransformSpace
@@ -18,28 +25,42 @@ namespace trc
 	class Transform
 	{
 	public:
-		void lookAt(Transform);
-		void lookAt();
+		void lookAt(const Transform& target);
+		void lookAt(const glm::vec3& target);
 
-		void rotate();
-		void rotateOnAxis();
-		void getRotation();
-		void setRotation();
+		void rotate(const glm::quat& rotation, TransformSpace space);
+		void rotateOnAxis(const glm::vec3& axis, Radian angle, TransformSpace space);
+		void setRotation(const glm::quat& rotation);
+		glm::quat getRotation() const;
 
-		void translate();
-		void getPosition();
-		void setPosition();
+		void translate(const glm::vec3& translation, TransformSpace space);
+		void setPosition(const glm::vec3& position);
+		glm::vec3 getPosition() const;
 
-		void scale();
-		void getScale();
-		void setScale();
+		void scale(const glm::vec3& scale, ScalingMode mode);
+		void setScale(const glm::vec3& scale);
+		glm::vec3 getScale() const;
 
-		void right();
-		void up();
-		void forward();
+		glm::vec3 right() const;
+		glm::vec3 up() const;
+		glm::vec3 forward() const;
+
+		void updateCachedTransform();
+		glm::mat4 computeTransform() const;
+
+		glm::mat4& getCachedTransformRef();
 
 	private:
+		void resetCachedTransform();
 
 		// children
+
+		std::optional<glm::mat4> m_cachedTransform;
+
+		glm::quat m_rotation;
+		glm::vec3 m_position;
+		glm::vec3 m_scale;
 	};
+
+	//constexpr auto size = sizeof(Transform);
 }  // namespace trc
